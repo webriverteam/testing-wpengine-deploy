c8f7c849e631984d4cc701a29c4ec09525b7a6d2
webriverteam <61460937+webriverteam@users.noreply.github.com>
2021-08-02 16:52:28 +0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -29,6 +29,7 @@ $basethemevar_error_search             = $option_fields['basethemevar_error_sear
 
 //Changed 404 file so master can be pushed
 // Changes done
+//another change
 ?>
 
 <div class="s-section">
